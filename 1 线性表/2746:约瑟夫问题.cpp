be476b9b36c5fbc043df2800f72bea6c8ad1104a
vwOvOwv <1271440868@qq.com>
2025-05-14 18:58:53 +0800

    update

--- 1 线性表/2746:约瑟夫问题.cpp
+++ 1 线性表/2746:约瑟夫问题.cpp
@@ -1,3 +1,12 @@
+// http://bailian.openjudge.cn/practice/2746/
+
+// n people (1, 2, ..., n) in a circle, every m-th person is killed
+// j(n, m) is the index of the last person left.
+// old index: 1,     2,     ..., m-1, m, m+1, ..., n
+// new index: n-m+1, n-m+2, ..., n-1, 0, 1,   ..., n-m (0 represents 'killed')
+// old = (new + m - 1) % n + 1
+// initial old: 1
+
 #include <iostream>
 
 using namespace std;
@@ -7,10 +16,10 @@ int n, m;
 int josephus(int n, int m){
     int res = 1;
     for(int i = 1; i <= n; i++){    // i: current number of people
-        res = (res + m) % i; 
+        res = (res + m - 1) % i + 1; 
         // cout << res + 1 << endl;
     }
-    return res + 1;
+    return res;
 }
 
 int main(){
--- 1 线性表/3254:约瑟夫问题No.2.cpp
+++ 1 线性表/3254:约瑟夫问题No.2.cpp
@@ -1,3 +1,7 @@
+// http://bailian.openjudge.cn/practice/3254/
+
+// simulate with list
+
 #include <cstddef>
 #include <iostream>
 
@@ -7,17 +11,52 @@ int n, p, m;
 
 class listNode{
 public:
-    int val;
+    int index;
     listNode *next;
     listNode(){}
-    listNode(int val=0, listNode *next=nullptr)
+    listNode(int index=0, listNode *next=nullptr){
+        this->index = index;
+        this->next = next;
+    }
 };
 
 void josephus(int n, int p, int m){
+    listNode * head = nullptr;
     listNode *cur = nullptr;
-    for (int i = 1; i<= n; i++){
-        listNode node;
+    listNode * start = nullptr;
+    for (int i = 1; i <= n; i++){
+        listNode *node = new listNode(i, NULL);
+        if(i == 1){
+            head = node;
+            cur = node;
+        }
+        else{
+            cur->next = node;
+            cur = node;
+        }
+
+        if(i == p)
+            start = node;
     }
+    cur->next = head;
+    // cout << head->index << endl;
+    // cout << cur->index << endl;
+    // cout << start->index << endl;
+    int killed_num = 0;
+    while(killed_num < n){
+        listNode *p = start;
+        for(int i = 0; i < m - 2; i++)
+            p = p->next;
+        start = p->next->next;
+        cout << p->next->index;
+        delete p->next;
+        p->next = start;
+        killed_num++;
+        if(killed_num != n){
+            cout <<',';
+        }
+    }
+    cout << endl;
 }
 
 int main(){
